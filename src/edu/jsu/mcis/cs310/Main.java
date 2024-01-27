fbd4681381f3e26494c311ad7c131cb91258a4b7
Denver <dwills2@stu.jsu.edu>
2024-01-27 13:41:20 -0600

    Completed Participation Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -17,7 +17,9 @@ public class Main {
     }
     
     public String reverse(String message) {
-        return message;
+        StringBuilder reversed = new StringBuilder(message);
+        reversed.reverse();
+        return reversed.toString();
     }
     
 }
